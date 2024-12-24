18edcf9e33e9a28430c1515138718dce1c3a4961
Bug1642 <hasancan1642@hotmail.com>
2024-12-24 13:35:03 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace StringExercise {
+    public class StudentInput {
+        public string[] NewArray() {
+            string [] musician = {"John","Mike","Joe","Kevin"};
+           return musician;
+        }
+    }
+    class Program{
+        static void Main (string [] args){
+            StudentInput music = new StudentInput();
+            string[] musicians = music.NewArray();
+            Console.WriteLine(musicians[0]);
+        }
+    }
+}
